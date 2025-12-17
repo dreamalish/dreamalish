03618a71f26744c8b326bae7e136c257c5a33145
dreamalish <dreamalish@gmail.com>
2025-12-17 09:57:50 -0600

    Update db.js

--- db.js
+++ db.js
@@ -23,29 +23,3 @@ sequelize.authenticate()
   });
 
 module.exports = sequelize;
-
-
-
-// sequelize.authenticate().then(() => {
-//     console.log("Connected to postgres database")
-// }, err => {
-//     console.error(err);
-// })
-
-//db associations setup
-// const userModel = require('./models/userModel')(sequelize);
-// const dreamModel = require('./models/dreamModel')(sequelize);
-// const commentModel = require('./models/commentModel')(sequelize);
-
-// // console.log("------------------------USER MODEL OUTPUT THINGY", userModel.hasMany(dreamModel));
-// dreamModel.belongsTo(userModel);
-// userModel.hasMany(dreamModel);
-
-// commentModel.belongsTo(dreamModel);
-// dreamModel.hasMany(commentModel);
-
-// commentModel.belongsTo(userModel);
-// userModel.hasMany(commentModel);
-
-// console.log("DB ASSOCIATIONS SET UP");
-module.exports = sequelize;
