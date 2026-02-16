766eb2a3ea1266f984708f4980eebd1bda69a381
Michael <hayesmv@yahoo.com>
2026-02-16 00:06:48 -0600

    likeModel.js

--- models/likeModel.js
+++ models/likeModel.js
@@ -1,6 +1,25 @@
 module.exports = (sequelize, DataTypes) => {
-    const Like = sequelize.define('Like', {
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-     
+    const Like = sequelize.define(
+      'Like',
+      {
+        userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        dreamId: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        }
+      },
+      {
+        indexes: [
+          {
+            unique: true,
+            fields: ['userId', 'dreamId']
+          }
+        ]
+      }
+    );
+  
+    return Like;
+  };
