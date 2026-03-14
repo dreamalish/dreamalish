3b4cc1fa2561b688ecaab31182485096c1c6f6e6
Michael <hayesmv@yahoo.com>
2026-03-14 03:20:15 -0500

    force true

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ if (process.env.NODE_ENV === 'production') {
 sequelize.authenticate()
   .then(() => {
     console.log('Connected to Postgres database');
-    return sequelize.sync({ alter: true });
+    return sequelize.sync({ force: true });
   })
   .then(() => {
     console.log('DB synced');
