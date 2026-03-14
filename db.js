56c3100cc1997a33d9ff3642936b34bf9564639a
Michael <hayesmv@yahoo.com>
2026-03-14 02:02:57 -0500

    db change

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
-  logging: false,
   dialectOptions: {
     ssl: {
       require: true,
