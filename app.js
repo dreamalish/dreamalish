5eaab4b2f61eb475efb7805dfb78ffc2805b21ae
Michael <hayesmv@yahoo.com>
2026-03-14 04:46:05 -0500

    force true

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ testDB();
 sequelize.authenticate()
   .then(() => {
     console.log('Connected to Postgres database');
-    return sequelize.sync({ alter: true });
+    return sequelize.sync({ force: true });
   })
   .then(() => {
     console.log('DB synced');
