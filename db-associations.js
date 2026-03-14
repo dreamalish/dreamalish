6cc54b2292ae461fdad403b5a96180609feedca8
Michael <hayesmv@yahoo.com>
2026-03-14 03:35:39 -0500

    we will see

--- db-associations.js
+++ db-associations.js
@@ -4,17 +4,15 @@ require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: isProduction
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      }
-    : {},
-  logging: false,
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  logging: false
 });
 
 
